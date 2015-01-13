bf827d7cfbaa275d1fdac42075ddfae7150f4ff6
James Leftley <jleftley@scottlogic.com>
2015-01-13 17:38:50 +0000

    Initial changes to Annotation tool

--- components/tools/annotation.js
+++ components/tools/annotation.js
@@ -3,95 +3,62 @@
 
     fc.tools.annotation = function() {
 
-        var index = 0,
-            xScale = d3.time.scale(),
-            yScale = d3.scale.linear(),
-            yLabel = '',
-            yValue = 0,
-            padding = 2,
-            formatCallout = function(d) { return d; };
-
-        var root = null,
-            line = null,
-            callout = null;
-
         var annotation = function(selection) {
 
-            root = selection.append('g')
-                .attr('id', 'annotation_' + index)
-                .attr('class', 'annotation');
-
-            line = root.append('line')
-                .attr('class', 'marker')
-                .attr('x1', xScale.range()[0])
-                .attr('y1', yScale(yValue))
-                .attr('x2', xScale.range()[1])
-                .attr('y2', yScale(yValue));
+            selection.each(function(data) {
+                var x = annotation.xScale.value,
+                    y = annotation.yScale.value,
+                    yValue = annotation.yValue.value();
 
 
-            callout = root.append('text')
-                .attr('class', 'callout')
-                .attr('x', xScale.range()[1] - padding)
-                .attr('y', yScale(yValue) - padding)
-                .attr('style', 'text-anchor: end;')
-                .text(yLabel + ': ' + formatCallout(yValue));
+                var container = d3.select(this)
+                    .selectAll('g.annotation')
+                    .data([data]);
+
+                container.enter()
+                    .append('g')
+                    .attr('class', 'annotation');
+
+                container.exit().remove();
+
+                var line = container.selectAll('line.marker').data([data]);
+
+                line.enter().append('line')
+                    .attr('class', 'marker');
+
+                line.attr('x1', x.range()[0])
+                    .attr('y1', y(yValue))
+                    .attr('x2', x.range()[1])
+                    .attr('y2', y(yValue));
+
+                line.exit().remove();
+
+                var label = container.selectAll('text.callout').data([data]);
+
+                label.enter().append('text')
+                    .attr('class', 'callout');
+
+                label.attr('x', x.range()[1])
+                    .attr('y', y(yValue))
+                    .attr('style', 'text-anchor: end;')
+                    .text(annotation.formatLabel.value(yValue));
+
+                label.exit().remove();
+
+                annotation.decorate.value(container);
+            });
+
         };
 
-        annotation.index = function(value) {
-            if (!arguments.length) {
-                return index;
-            }
-            index = value;
-            return annotation;
-        };
+        annotation.xScale = fc.utilities.property(d3.time.scale());
 
-        annotation.xScale = function(value) {
-            if (!arguments.length) {
-                return xScale;
-            }
-            xScale = value;
-            return annotation;
-        };
+        annotation.yScale = fc.utilities.property(d3.scale.linear());
 
-        annotation.yScale = function(value) {
-            if (!arguments.length) {
-                return yScale;
-            }
-            yScale = value;
-            return annotation;
-        };
+        annotation.yValue = fc.utilities.functorProperty(fc.utilities.valueAccessor('close'));
 
-        annotation.yValue = function(value) {
-            if (!arguments.length) {
-                return yValue;
-            }
-            yValue = value;
-            return annotation;
-        };
+        annotation.formatLabel = fc.utilities.property(d3.scale.linear());
 
-        annotation.yLabel = function(value) {
-            if (!arguments.length) {
-                return yLabel;
-            }
-            yLabel = value;
-            return annotation;
-        };
-
-        annotation.padding = function(value) {
-            if (!arguments.length) {
-                return padding;
-            }
-            padding = value;
-            return annotation;
-        };
-
-        annotation.formatCallout = function(value) {
-            if (!arguments.length) {
-                return formatCallout;
-            }
-            formatCallout = value;
-            return annotation;
-        };
+        annotation.decorate = fc.utilities.property(fc.utilities.fn.noop);
 
         return annotation;
     };
---
+++ visual-tests/test-scripts/tools/annotation.js
@@ -0,0 +1,96 @@
+(function(d3, fc) {
+    'use strict';
+
+    var data = fc.utilities.dataGenerator()
+        .seedDate(new Date(2014, 1, 1))
+        .randomSeed('12345')
+        .generate(50);
+
+    var chart = d3.select('#annotation'),
+        chartLayout = fc.utilities.chartLayout();
+
+    chart.call(chartLayout);
+
+    // Create scale for x axis
+    var dateScale = fc.scale.dateTime()
+        .domain(fc.utilities.extent(data, 'date'))
+        .range([0, chartLayout.getPlotAreaWidth()])
+        .nice();
+
+    // Create scale for y axis
+    var priceScale = fc.scale.linear()
+        .domain(fc.utilities.extent(data, ['high', 'low']))
+        .range([chartLayout.getPlotAreaHeight(), 0])
+        .nice();
+
+    // Create the axes
+    var dateAxis = d3.svg.axis()
+        .scale(dateScale)
+        .orient('bottom')
+        .ticks(5);
+
+    var priceAxis = d3.svg.axis()
+        .scale(priceScale)
+        .orient('right')
+        .ticks(5);
+
+    // Add the axes to the chart
+    chartLayout.getAxisContainer('bottom').call(dateAxis);
+    chartLayout.getAxisContainer('right').call(priceAxis);
+
+    // Create the OHLC series
+    var ohlc = fc.series.ohlc()
+        .xScale(dateScale)
+        .yScale(priceScale);
+
+    // Add the primary OHLC series
+    chartLayout.getPlotArea().append('g')
+        .attr('class', 'series')
+        .datum(data)
+        .call(ohlc);
+
+    // Create the annotations
+    var annotation100 = fc.tools.annotation()
+        .xScale(dateScale)
+        .yScale(priceScale)
+        .yValue(100);
+
+    var annotation102 = fc.tools.annotation()
+        .xScale(dateScale)
+        .yScale(priceScale)
+        .yValue(102);
+
+    var annotationDecimal = fc.tools.annotation()
+        .xScale(dateScale)
+        .yScale(priceScale)
+        .yValue(100.675)
+        .decorate(function(selection) {
+            selection.selectAll('line')
+                .style('stroke', 'red');
+            selection.selectAll('text')
+                .attr('x', dateScale.range()[1] - 20);
+        })
+        .formatLabel(function(d) { return d3.format('.3f')(d); });
+
+    // Add it to the chart
+    chartLayout.getPlotArea()
+        .append('g')
+        .attr('id', 'annotation100')
+        .call(annotation100);
+
+    chartLayout.getPlotArea()
+        .append('g')
+        .attr('id', 'annotation102')
+        .call(annotation102);
+
+    chartLayout.getPlotArea()
+        .append('g')
+        .attr('id', 'annotationDecimal')
+        .call(annotationDecimal);
+
+    // Update an annotation
+    annotationDecimal.yValue(101.675);
+    chartLayout.getPlotArea().select('#annotationDecimal')
+        .call(annotationDecimal);
+
+})(d3, fc);
