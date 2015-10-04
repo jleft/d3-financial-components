a77add1ba0c1cd7505a75078aeeafff7222b175e
James Leftley <jleftley@scottlogic.com>
2015-10-04 14:31:23 +0100

    Update multi to always order series containers in the same order as specified in the series array

--- tests/series/multiSpec.js
+++ tests/series/multiSpec.js
@@ -109,4 +109,123 @@ describe('multi', function() {
         expect(seriesA.calls.mostRecent().args[0].node()).toBe(seriesAContainer);
         expect(seriesB.calls.mostRecent().args[0].node()).toBe(seriesBContainer);
     });
+
+    describe('ordering of series containers', function() {
+
+        var seriesA, seriesB, seriesC;
+
+        beforeEach(function() {
+            seriesA = createSeriesSpy();
+            seriesA.id = 'seriesA';
+
+            seriesB = createSeriesSpy();
+            seriesB.id = 'seriesB';
+
+            seriesC = createSeriesSpy();
+            seriesC.id = 'seriesC';
+
+            multi.key(function(series) {
+                return series.id;
+            });
+        });
+
+
+        it('should order series containers in an order consistent with the series', function() {
+
+            multi.series([seriesA, seriesB]);
+
+            container.call(multi);
+
+            var seriesAContainer = seriesA.calls.mostRecent().args[0].node(),
+                seriesBContainer = seriesB.calls.mostRecent().args[0].node();
+
+            expect(element.childNodes.length).toBe(2);
+            expect(element.childNodes[0]).toBe(seriesAContainer);
+            expect(element.childNodes[1]).toBe(seriesBContainer);
+        });
+
+        it('should insert new series containers in an order consistent with the series', function() {
+
+            multi.series([seriesA, seriesB]);
+
+            container.call(multi);
+
+            var seriesAContainer = seriesA.calls.mostRecent().args[0].node(),
+                seriesBContainer = seriesB.calls.mostRecent().args[0].node();
+
+            expect(element.childNodes.length).toBe(2);
+            expect(element.childNodes[0]).toBe(seriesAContainer);
+            expect(element.childNodes[1]).toBe(seriesBContainer);
+
+            multi.series([seriesA, seriesC, seriesB]);
+
+            container.call(multi);
+
+            seriesAContainer = seriesA.calls.mostRecent().args[0].node();
+            seriesBContainer = seriesB.calls.mostRecent().args[0].node();
+            var seriesCContainer = seriesC.calls.mostRecent().args[0].node();
+
+            expect(element.childNodes.length).toBe(3);
+            expect(element.childNodes[0]).toBe(seriesAContainer);
+            expect(element.childNodes[1]).toBe(seriesCContainer);
+            expect(element.childNodes[2]).toBe(seriesBContainer);
+        });
+
+        it('should order series containers in an order consistent with the series when removing a series', function() {
+
+            multi.series([seriesA, seriesB, seriesC]);
+
+            container.call(multi);
+
+            var seriesAContainer = seriesA.calls.mostRecent().args[0].node(),
+                seriesBContainer = seriesB.calls.mostRecent().args[0].node(),
+                seriesCContainer = seriesC.calls.mostRecent().args[0].node();
+
+            expect(element.childNodes.length).toBe(3);
+            expect(element.childNodes[0]).toBe(seriesAContainer);
+            expect(element.childNodes[1]).toBe(seriesBContainer);
+            expect(element.childNodes[2]).toBe(seriesCContainer);
+
+            multi.series([seriesA, seriesC]);
+
+            container.call(multi);
+
+            seriesAContainer = seriesA.calls.mostRecent().args[0].node();
+            seriesCContainer = seriesC.calls.mostRecent().args[0].node();
+
+            expect(element.childNodes.length).toBe(2);
+            expect(element.childNodes[0]).toBe(seriesAContainer);
+            expect(element.childNodes[1]).toBe(seriesCContainer);
+        });
+
+        it('should re-order series containers in an order consistent with the series array', function() {
+
+            multi.series([seriesA, seriesB, seriesC]);
+
+            container.call(multi);
+
+            var seriesAContainer = seriesA.calls.mostRecent().args[0].node(),
+                seriesBContainer = seriesB.calls.mostRecent().args[0].node(),
+                seriesCContainer = seriesC.calls.mostRecent().args[0].node();
+
+            expect(element.childNodes.length).toBe(3);
+            expect(element.childNodes[0]).toBe(seriesAContainer);
+            expect(element.childNodes[1]).toBe(seriesBContainer);
+            expect(element.childNodes[2]).toBe(seriesCContainer);
+
+            multi.series([seriesC, seriesA, seriesB]);
+
+            container.call(multi);
+
+            seriesAContainer = seriesA.calls.mostRecent().args[0].node();
+            seriesBContainer = seriesB.calls.mostRecent().args[0].node();
+            seriesCContainer = seriesC.calls.mostRecent().args[0].node();
+
+            expect(element.childNodes.length).toBe(3);
+            expect(element.childNodes[0]).toBe(seriesCContainer);
+            expect(element.childNodes[1]).toBe(seriesAContainer);
+            expect(element.childNodes[2]).toBe(seriesBContainer);
+        });
+
+    });
 });
