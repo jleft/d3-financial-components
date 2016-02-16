fbfaf2a4f20a606d68ec71709ef015bf64776124
James Leftley <jleftley@scottlogic.com>
2016-02-16 18:35:23 +0000

    WIP

--- src/util/fractionalBarWidth.js
+++ src/util/fractionalBarWidth.js
@@ -1,4 +1,5 @@
 import d3 from 'd3';
+import {defined} from '../util/fn';
 
 // the barWidth property of the various series takes a function which, when given an
 // array of x values, returns a suitable width. This function creates a width which is
@@ -6,7 +7,14 @@ import d3 from 'd3';
 // by the given factor
 export default function(fraction) {
 
-    return function(pixelValues) {
+    var scale,
+        accessor,
+        definedFilter;
+
+    var fractionalBarWidth = function(data) {
+        var filteredData = data.filter(definedFilter);
+
+        var pixelValues = data.map(function(d, i) { return scale(accessor(d, i)); });
         // return some default value if there are not enough datapoints to compute the width
         if (pixelValues.length <= 1) {
             return 10;
@@ -23,4 +31,28 @@ export default function(fraction) {
         var minDistance = d3.min(neighbourDistances);
         return fraction * minDistance;
     };
+
+    fractionalBarWidth.defined = function(x) {
+        if (!arguments.length) {
+            return definedFilter;
+        }
+        definedFilter = x;
+        return fractionalBarWidth;
+    };
+    fractionalBarWidth.scale = function(x) {
+        if (!arguments.length) {
+            return scale;
+        }
+        scale = x;
+        return fractionalBarWidth;
+    };
+    fractionalBarWidth.accessor = function(x) {
+        if (!arguments.length) {
+            return accessor;
+        }
+        accessor = x;
+        return fractionalBarWidth;
+    };
+
+    return fractionalBarWidth;
 }
